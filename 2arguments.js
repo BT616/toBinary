20b25e210c54cac443fc9916fc38ee2c6432647f
BT616 <110855674+BT616@users.noreply.github.com>
2023-03-24 18:15:41 -0700

    endswith() would be easier

--- 2arguments.js
+++ 2arguments.js
@@ -1,7 +1,7 @@
 //Complete the solution so that it returns true if the first argument(string)
 // passed in ends with the 2nd argument (also a string).
 
-// .substr() returns a portion of the string, 
+// .substr() returns a portion of the string, "endswith()" would also be helpful in this situation 
 
 function solution(str, ending){
   // TODO: complete
