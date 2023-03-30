1df6bcad98a6fd9652a4fde8321bd0361842b99c
BT616 <110855674+BT616@users.noreply.github.com>
2023-03-30 15:09:30 -0700

    get initials of two words

---
+++ twowords.js
@@ -0,0 +1,18 @@
+function abbrevName(name){
+
+    // code away
+return name.replace(/\b(\w)\w+/g, '$1')
+        .replace(/\s/g,'.')
+        .replace(/\.$/,'')
+        .toUpperCase();
+}
+
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
