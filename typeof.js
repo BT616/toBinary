f81ebfe7c283c5c55d66c53a4fc2394451cf0854
BT616 <110855674+BT616@users.noreply.github.com>
2023-04-01 18:31:48 -0700

    use type of

---
+++ typeof.js
@@ -0,0 +1,13 @@
+function problem(x){
+    //your code here
+  if(typeof(x)=='number'){
+      return x*50+6
+      
+    }else{
+      return 'Error'
+    }
+    
+  }
+
+ // Make a function that returns the value multiplied by 50 and increased by 6.
+   //If the value entered is a string it should return "Error".
