37aeb1370f72cf983cc2e2f1fc212f93a63339a6
BT616 <110855674+BT616@users.noreply.github.com>
2023-04-07 14:32:51 -0700

    parseint

---
+++ OneandZero.js
@@ -0,0 +1,10 @@
+const binaryArrayToNumber = arr => {
+    // your code
+    const binary = arr.join("");
+    return parseInt(binary,2)
+    
+  };
+
+//   Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
