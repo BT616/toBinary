7b65c374190c1028fc54b21bea14b03d987944eb
BT616 <110855674+BT616@users.noreply.github.com>
2023-04-14 17:52:17 -0700

    .split(#)[0]

---
+++ removeAnchor.js
@@ -0,0 +1,7 @@
+function removeUrlAnchor(url){
+    // TODO: complete
+    
+    return url.split("#")[0]
+  }
+
+  //Complete the function/method so that it returns the url with anything after the anchor (#) removed.
