c6c98553a12a46873942ffbb259113a6caacb952
BT616 <110855674+BT616@users.noreply.github.com>
2023-04-19 21:46:34 -0700

    typeof string

---
+++ filterString.js
@@ -0,0 +1,11 @@
+
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+   return l.filter(string=> typeof string != 'string');
+   
+  }
+
+
+
+//In this kata you will create a function that takes a list of non-negative integers and strings 
+//and returns a new list with the strings filtered out.
