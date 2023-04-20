c60031c06c69663bae4046c0002a4670dca61868
BT616 <110855674+BT616@users.noreply.github.com>
2023-04-19 20:55:00 -0700

    even or odd

---
+++ evenOrOdd.js
@@ -0,0 +1,15 @@
+function oddOrEven(array) {
+    var result = 0;
+    for (var i = 0; i < array.length; i++)
+    {result+=array[i];}
+    if (result%2 == 0)
+    {return "even";}
+    else{return "odd";}
+  }
+
+  //Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
---
+++ replaceVowel.js
@@ -0,0 +1,5 @@
+replace =s => 
+  //coding and coding....
+   s.replace(/[aeiou]/gi,'!')
+
+ //  Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
