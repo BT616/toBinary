f085848d972cba22aa44e4ee7e46f8bbe059f274
BT616 <110855674+BT616@users.noreply.github.com>
2023-08-06 14:05:41 -0700

    names.length

--- namesIf.js
+++ namesIf.js
@@ -3,4 +3,8 @@
 //We want to create the text that should be displayed next to such an item.
 //Implement the function which takes an array containing the names of people that like an item.
 //It must return the display text as shown in the examples:
+function likes (names) {
+if (names.length === 0) {
+    return 'no one likes this';
 
+}}
