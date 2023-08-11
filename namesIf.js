58b67a34d79b79fbbc630bb35a754dc3dc7dea4b
BT616 <110855674+BT616@users.noreply.github.com>
2023-08-11 10:51:02 -0700

    length =2, names.length not bob

--- namesIf.js
+++ namesIf.js
@@ -6,12 +6,12 @@
 function likes (names) {
 if (names.length === 0) {
     return 'no one likes this';
-} else if(names === 1){
+} else if(names.length === 1){
     return `${names[0]} likes this`
-} else if(bob === 2){
+} else if(names.length === 2){
     return `${names[0]}, ${names[1]} and ${names[2]} like this`
   }
-  else if(bob>3){
+  else if(names.length>3){
     return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
   }
 }
