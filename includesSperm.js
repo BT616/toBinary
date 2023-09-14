5afbb8e3f7c3d859580e4e5487280f5e6c99d4f3
BT616 <110855674+BT616@users.noreply.github.com>
2023-09-13 23:09:01 -0700

    again

---
+++ includesSperm.js
@@ -0,0 +1,12 @@
+// If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter.";
+//  If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
+
+function chromosomeCheck(sperm) {
+  
+    if(sperm.charAt(1) =="Y"){
+     return "Congratulations! You're going to have a son."
+    } 
+    return "Congratulations! You're going to have a daughter."
+   }
+
+   
